6f36ba830c533c5303c201ee3c349ed7a32983b2
Oleg <oleg0kofficial@gmail.com>
2026-05-02 02:32:20 +0300

    fix: jwt sign types

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,7 +1,8 @@
-import jwt from "jsonwebtoken";
+import jwt, { SignOptions } from "jsonwebtoken";
 
 const SECRET = process.env.JWT_SECRET as string;
-const EXPIRES_IN = process.env.JWT_EXPIRES_IN ?? "7d";
+const EXPIRES_IN = (process.env.JWT_EXPIRES_IN ??
+  "7d") as SignOptions["expiresIn"];
 
 export const signToken = (payload: object) =>
   jwt.sign(payload, SECRET, { expiresIn: EXPIRES_IN });
